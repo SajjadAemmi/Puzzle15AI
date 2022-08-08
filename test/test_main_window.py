09cd0dec27bc77e0db18024b6486166bbbcafd3e
Sajjad Aemmi <sajjadaemmi@gmail.com>
2022-08-08 20:04:06 +0430

    add tests

---
+++ test/test_main_window.py
@@ -0,0 +1,3 @@
+from puzzle15ai.main_window import MainWindow
+
+main_window = MainWindow()
---
+++ test/test_tree.py
@@ -0,0 +1,3 @@
+from puzzle15ai.tree import Tree
+
+tree = Tree(start_state=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]])
